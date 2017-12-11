173c8b5082258c1a8fc4f0ac9ce4339b6696fc07
jackwhihongtan <18tanj98@millshighsch.org>
2017-12-11 09:41:50 -0800

    Changes to frac calc

---
+++ NewFracCalc/src/fracCalc/Fraction.java
@@ -0,0 +1,13 @@
+package fracCalc;
+
+public class Fraction {
+ private int numerator;
+ private int denominator;
+ private int whole;
+ private int sign;
+	public Fraction(int newNumerator, int newDenominator, int newWhole) {
+		numerator = newNumerator;
+		denominator = newDenominator;
+		whole = newWhole;
+	}
+}
