3fa0d47368b700e24861146538e6b91f29be0769
jackt <jackt@LAPTOP-4EVBE05A.attlocal.net>
2017-12-18 17:07:03 -0800

    More work

--- NewFracCalc/src/fracCalc/FracCalc.java
+++ NewFracCalc/src/fracCalc/FracCalc.java
@@ -4,92 +4,94 @@ import java.util.Scanner;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
-    	boolean run = true;
-      	while (run = true) {
-      	Scanner sc = new Scanner(System.in);
-      	System.out.println("Please input the fractions:");
-      	String userInput = sc.nextLine();
-      	String answer = produceAnswer(userInput);
-      	System.out.println(answer);
-      	System.out.println("Are you done yet?");
-      	String finish = sc.next();
-    }
-    }
-      	public static String[] Parse(String Parse)
-        { 
-        	String[] lastValue = Parse.split(" ");
-            return lastValue;
-        }
-      	 public static String Oper1(String[] lastvalue) {
-      	  	String oper1 = lastvalue[0];
-      	  	return oper1;
-      	  }
-      	  
-      	  public static String Oper2(String[] lastvalue) {
-      	  	int arrlen = lastvalue.length;
-      	  	String oper2 = lastvalue[arrlen - 1];
-      	  	return oper2;
-      	  }
-      	  
-      	  public static int[] splitOper(String input) {
-      	  	
-      	   	int[] operon = new int[3];
-      	   	
-      	   	if (input.indexOf('_') != -1) {
-      	   		operon[0] = Integer.parseInt(input.substring(0,input.indexOf("_")));
-      	   			if(input.indexOf('/') != -1) {
-      	   				operon[1] = Integer.parseInt(input.substring(input.indexOf("_") + 1, input.indexOf("/")));
-      	   				operon[2] = Integer.parseInt(input.substring(input.indexOf("/") + 1));
-      	   		}
-      	   		return operon;
-      	   	}
-      	   	else if(input.indexOf('/') != -1) {
-      	   		operon[0] = 0;
-      	   		operon[1] = Integer.parseInt(input.substring(0, input.indexOf("/")));
-      	   		operon[2] = Integer.parseInt(input.substring(input.indexOf("/") + 1));
-      	   		return operon;
-      	   	} else {
-      	   		operon[0] = Integer.parseInt(input.substring(0));
-      	   		operon[1] = 0;
-      	   		operon[2] = 1;
-      	   		return operon;
-      	   	}
-      	   }
-    public static String produceAnswer(String input){ 
-        int[] fraction = splitOper(Oper1(Parse(input)));
-        int[] fraction2 = splitOper(Oper2(Parse(input)));
-        String[] operator = Parse(input);
+	public static void main(String[] args) {
+		boolean run = true;
+		while (run = true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Please input the fractions:");
+			String userInput = sc.nextLine();
+			String answer = produceAnswer(userInput);
+			System.out.println(answer);
+			System.out.println("Are you done yet?");
+			String finish = sc.next();
+		}
+	}
+
+	public static String[] Parse(String Parse) {
+		String[] lastValue = Parse.split(" ");
+		return lastValue;
+	}
+
+	public static String Oper1(String[] lastvalue) {
+		String oper1 = lastvalue[0];
+		return oper1;
+	}
+
+	public static String Oper2(String[] lastvalue) {
+		int arrlen = lastvalue.length;
+		String oper2 = lastvalue[arrlen - 1];
+		return oper2;
+	}
+
+	public static int[] splitOper(String input) {
+
+		int[] operon = new int[3];
+
+		if (input.indexOf('_') != -1) {
+			operon[0] = Integer.parseInt(input.substring(0, input.indexOf("_")));
+			if (input.indexOf('/') != -1) {
+				operon[1] = Integer.parseInt(input.substring(input.indexOf("_") + 1, input.indexOf("/")));
+				operon[2] = Integer.parseInt(input.substring(input.indexOf("/") + 1));
+			}
+			return operon;
+		} else if (input.indexOf('/') != -1) {
+			operon[0] = 0;
+			operon[1] = Integer.parseInt(input.substring(0, input.indexOf("/")));
+			operon[2] = Integer.parseInt(input.substring(input.indexOf("/") + 1));
+			return operon;
+		} else {
+			operon[0] = Integer.parseInt(input.substring(0));
+			operon[1] = 0;
+			operon[2] = 1;
+			return operon;
+		}
+	}
+	public static int[] toImproperFrac(int[] fraction) {
+		if (fraction[0] < 0) {
+			fraction[0] = fraction[0] * -1;
+			fraction[1] = ((fraction[0] * fraction[2]) + fraction[1]);
+			fraction[0] = 0;
+			fraction[1] = fraction[1] * -1;
+			return fraction;
+		} else {
+			fraction[1] = ((fraction[0] * fraction[2]) + fraction[1]);
+			fraction[0] = 0;
+			return fraction;
+		}
+	}
+	public static String produceAnswer(String input) {
+		int[] fraction = toImproperFrac(splitOper(Oper1(Parse(input))));
+		int[] fraction2 = toImproperFrac(splitOper(Oper2(Parse(input))));
+		String[] operator = Parse(input);
 		String operatorSign = operator[1];
-        Fraction Operon1 = new Fraction(fraction[1], fraction[2], fraction[0]);
-        Fraction Operon2 = new Fraction(fraction2[1], fraction2[2], fraction2[0]);
+		Fraction Operon1 = new Fraction(fraction[0], fraction[1], fraction[2]);
+		Fraction Operon2 = new Fraction(fraction2[0], fraction2[1], fraction2[2]);
 		if (operatorSign.equals("+")) {
 			Fraction additionAns = Operon1.addition(Operon2);
-			
-			return finalanswer;
-		} else if(operatorSign.equals("-")) {
-			fraction2[1] = fraction2[1] * -1;
-			answer = addition(fraction1, fraction2);
-			String finalanswer = toString(answer);
-			return finalanswer;
-			} else if(operatorSign.equals("/")) {
-				int switchNum = fraction2[2];
-		    	fraction2[2] = fraction2[1];
-		    	fraction2[1] = switchNum;
-		    	answer = multiply(fraction1, fraction2);
-		    	String finalanswer = toString(answer);
-		    	return finalanswer;
-			} else {
-				answer = multiply(fraction1, fraction2);
-				String finalanswer = toString(answer);
-				return finalanswer;
-			}
+			String answer = additionAns.toString(additionAns);
+			return answer;
+		} else if (operatorSign.equals("-")) {
+			Fraction subtractAns = Operon1.subtract(Operon2);
+			String answer = subtractAns.toString(subtractAns);
+			return answer;
+		} else if (operatorSign.equals("/")) {
+			Fraction divideAns = Operon1.divide(Operon2);
+			String answer = divideAns.toString(divideAns);
+			return answer;
+		} else {
+			Fraction multiplyAns = Operon1.multiply(Operon2);
+			String answer = multiplyAns.toString(multiplyAns);
+			return answer;
 		}
-  
-        return "";
-    }
-
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    
+	}
 }
--- NewFracCalc/src/fracCalc/Fraction.java
+++ NewFracCalc/src/fracCalc/Fraction.java
@@ -3,78 +3,156 @@ package fracCalc;
 import java.util.Arrays;
 
 public class Fraction {
- private int numerator;
- private int denominator;
- private int whole;
- private int sign;
-	public Fraction(int newNumerator, int newDenominator, int newWhole) {
+	private int numerator;
+	private int denominator;
+	private int whole;
+	private int sign;
+
+	public Fraction(int newWhole, int newNumerator, int newDenominator) {
 		numerator = newNumerator;
 		denominator = newDenominator;
 		whole = newWhole;
 	}
-	public String toString() {
-		return ("whole:"+ whole +" numerator:"+ numerator + " denominator:"+ denominator +"");
-	}
+
 	public void toMixedNumber() {
 		numerator = (whole * denominator) + numerator;
 	}
-	public void toImproperFrac() {
-		whole = numerator / denominator;
-		numerator = numerator % denominator;
-	}
+
 	public Fraction addition(Fraction Operon2) {
 		numerator = (numerator * Operon2.denominator);
 		Operon2.numerator = (Operon2.numerator * denominator);
-	  	denominator = (denominator * Operon2.denominator);
-	  	whole = numerator + Operon2.numerator;
-	  	Fraction additionAns = new Fraction(whole, numerator, denominator);
-	  	return additionAns;
+		denominator = (denominator * Operon2.denominator);
+		numerator = numerator + Operon2.numerator;
+		Fraction additionAns = new Fraction(whole, numerator, denominator);
+		System.out.println(whole);
+		System.out.println(numerator);
+		System.out.println(denominator);
+		return additionAns;
 	}
-	public void multiply(int[] fraction2) {
-		whole = numerator * fraction2[1];
-	  	fraction2[0] = denominator * fraction2[2];
-	  	numerator = whole;
-	  	whole = 0;
-	  	denominator = fraction2[0];
+
+	public Fraction subtract(Fraction Operon2) {
+		numerator = (numerator * Operon2.denominator);
+		Operon2.numerator = (Operon2.numerator * -1 * denominator);
+		denominator = (denominator * Operon2.denominator);
+		numerator = numerator + Operon2.numerator;
+		Fraction subtractAns = new Fraction(whole, numerator, denominator);
+		return subtractAns;
 	}
+
+	public Fraction multiply(Fraction Operon2) {
+		numerator= numerator * Operon2.numerator;
+		System.out.println(numerator);
+		System.out.println(whole);
+		denominator = denominator * Operon2.denominator;
+		System.out.println(Operon2.whole);
+		whole = 0;
+		System.out.println(whole);
+		Fraction multiplyAns = new Fraction(whole, numerator, denominator);
+		System.out.println(whole);
+		System.out.println(multiplyAns);
+		return multiplyAns;
+	}
+
+	public Fraction divide(Fraction Operon2) {
+		int switchNum = Operon2.denominator;
+		Operon2.denominator = Operon2.numerator;
+		Operon2.numerator = switchNum;
+		whole = numerator * Operon2.numerator;
+		Operon2.whole = denominator * Operon2.denominator;
+		numerator = whole;
+		whole = 0;
+		denominator = Operon2.whole;
+		Fraction divideAns = new Fraction(whole, numerator, denominator);
+		return divideAns;
+	}
+
 	public String toString(Fraction reduce) {
-		 if(numerator > denominator || absoluteValue(numerator) > denominator) {
-		  		reduce = reduceFraction(reduce);
-		  		String convert = "" + whole +"_"+ numerator +"/"+ denominator + "";
-		  		return convert;			
-		  	} else if (numerator == 0) {
-		  		return "" + whole + "";
-		  	} else if (denominator == 1) {
-		  		String convert = "" + numerator +  "";
-		  		return convert;
-		  	}
-		  	else {
-		  		reduce = reduceFraction(reduce);
-		  		String convert = "" + numerator +"/"+denominator+ "";
-		  		return convert;
-		  	}
-		  } 
-		 public Fraction reduceFraction(Fraction reduce) {
-			  if (numerator < 0) {
-				  numerator = numerator * -1;
-				  int gcf = gcf(numerator, denominator);
-				  numerator = numerator / gcf;
-				denominator = denominator / gcf;
-				whole = numerator / denominator;
-				numerator = numerator % denominator;
-				return answer;
-		  	} else {
-		  		int gcf = gcf(numerator, denominator);
-		  		System.out.println(gcf);
-		  	  numerator = numerator / gcf;
-		
-		  		denominator = denominator / gcf;
-		  	
-		  		whole = numerator / denominator;
-		  		numerator = numerator % denominator;
-		  		
-		  		return answer;
-		  	}
+		if (denominator == 1) {
+			String convert = "" + numerator + "";
+			return convert;
+		} else if (denominator == -1) {
+			numerator = numerator * -1;
+			String convert = "" + numerator + "";
+			return convert;
+		} else if (numerator == 0) {
+			return "0";
+		} else if (numerator % denominator == 0) {
+			reduce = reduceFraction(reduce);
+			String convert = "" + whole + "";
+			return convert;
+		} else if (numerator < 0 && denominator < 0) {
+			numerator = numerator * -1;
+			denominator = denominator * -1;
+			reduce = reduceFraction(reduce);
+			String convert = "" + whole + "_" + numerator + "/" + denominator + "";
+			return convert;
+		} else if (numerator > denominator || absoluteValue(numerator) > denominator) {
+			reduce = reduceFraction(reduce);
+			if (whole == 0) {
+				return "" + numerator + "/" + denominator + "";
+			}
+			String convert = "" + whole + "_" + numerator + "/" + denominator + "";
+			return convert;
+		} else {
+			reduce = reduceFraction(reduce);
+			String convert = "" + numerator + "/" + denominator + "";
+			return convert;
+		}
 	}
-	
+
+	public Fraction reduceFraction(Fraction reduce) {
+		if (numerator < 0) {
+			int gcf = gcf(numerator, denominator);
+			if (gcf < 0) {
+				gcf *= -1;
+			}
+			numerator = numerator / gcf;
+			denominator = denominator / gcf;
+			whole = numerator / denominator;
+			if (whole < 0) {
+				numerator *= -1;
+			}
+			if (denominator < 0) {
+				denominator *= -1;
+			}
+			numerator = numerator % denominator;
+			return reduce;
+		} else {
+			int gcf = gcf(numerator, denominator);
+			numerator = numerator / gcf;
+			denominator = denominator / gcf;
+			whole = numerator / denominator;
+			if (denominator < 0) {
+				denominator *= -1;
+			}
+			if (whole < 0 && numerator < 0) {
+				numerator *= -1;
+			}
+			numerator = numerator % denominator;
+			return reduce;
+		}
+	}
+
+	public static int gcf(int num1, int num2) {
+		int gcf = 0;
+		int numerator = num1;
+		int denom = num2;
+		num1 = absoluteValue(numerator); // made values positive to make method work
+		num2 = absoluteValue(denom);
+		while (numerator != 0 && denom != 0) { // test for both zero for efficiency of code
+			int c = denom; // declare new variable
+			denom = numerator % denom;
+			numerator = c;
+		}
+		gcf = numerator + denom;
+		return gcf;
+	}
+
+	public static int absoluteValue(int number) {
+		if (number > 0) {
+			return number;
+		} else {
+			return number * -1;
+		}
+	}
 }
